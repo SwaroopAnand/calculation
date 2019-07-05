c271c6870130f2569c856cdf09af17f5de2c99a5
swaroop <swaroopanand26263@gmail.com>
2019-07-05 17:03:25 +0530

    test file

---
+++ tests/test_calculator.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+from testing_pro.Calculator_Project.Calculator import CalcMachine
+
+class CalculatorTest(TestCase):
+    def setUp(self):
+        self.calc = CalcMachine()
+
+    def test_calc_div(self):
+        self.assertEqual(5,self.calc.div(20,4))
+
+    # def test_calc_div_str(self):
+    #     self.assertRaises(Exception, self.calc.div('this','fails'))
+
+
+if __name__ == '__main__':
+    unittest.main()
